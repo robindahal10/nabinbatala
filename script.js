29702cd31e2e2dcd18908e367e4b9761ea108e1c
robindahal10 <rabindahal2003@gmail.com>
2026-01-23 20:55:07 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const toggle = document.getElementById("toggle");
 const body = document.body;
 
 toggle.addEventListener("click", () => {
-  body.classList.toggle("light");
   body.classList.toggle("dark");
+  body.classList.toggle("light");
   toggle.textContent = body.classList.contains("dark") ? "🌙" : "☀️";
 });
