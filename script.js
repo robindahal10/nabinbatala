d196c526ccbfcccae01c71842e6aee0376b0aec7
robindahal10 <rabindahal2003@gmail.com>
2026-01-29 21:31:32 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ toggle.addEventListener("click", () => {
   body.classList.toggle("light");
   toggle.textContent = body.classList.contains("dark") ? "🌙" : "☀️";
 });
+<script>
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  setInterval(() => {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+  }, 3000); // change image every 3 seconds
+</script>
